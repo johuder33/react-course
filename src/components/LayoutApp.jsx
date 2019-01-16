d2a50931d30697fd1b1f7664a840664371ecd2fd
José Simo <simo7ven@gmail.com>
2019-01-16 19:50:47 -0300

    structure layout (#2)

--- src/components/LayoutApp.jsx
+++ src/components/LayoutApp.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import { WithAuthChecker } from "./withAuthChecker";
 import MenuAppBar from "../components/AppBar";
-
-
+import {
+  Grid
+} from "@material-ui/core";
+import Content from "./Content";
 class LayoutApp extends Component {
   render() {
-    return <MenuAppBar/>;
+    return (<Grid container>
+              <Grid item xs={2}>
+              2
+              </Grid>
+              <Grid item container xs={10}>
+                  <Grid item xs={12}>
+                    titulo
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Content/>
+                  </Grid>
+
+              </Grid>
+
+           </Grid>);
+    // return <MenuAppBar/>;
   }
 }
 
